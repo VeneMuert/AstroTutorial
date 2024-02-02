7b4e29623bfabb98e281899cbe84a6a57c19eaa3
amitkaill <amitkaill1989@gmail.com>
2024-02-02 00:41:48 -0400

    astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://amitkaillarmas.netlify.app"
+  site: "https://amitkaillarmas.netlify.app",
+  integrations: [preact()]
 });
