69ed47e1cc2dff5027e3979b0ad0cb59c019755a
Amitkaill Armas <amitkaill1989@gmail.com>
2024-02-24 11:35:30 -0400

    Update astro.config.mjs
    regresando

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://venemuert.github.io/",
-  base: "/AstroTutorial"
+  site: "https://amitkaillarmas.netlify.app",
+  integrations: [preact()]
 });
